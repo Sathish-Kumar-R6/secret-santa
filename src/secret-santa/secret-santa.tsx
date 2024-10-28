fae00e62ab38b71d5c9338a181a2a6926906e82f
SATHISH-ASUS\sathi <sathish662001@gmail.com>
2024-10-29 01:45:08 +0530

    fix(application): improve table and css styles

--- src/secret-santa/secret-santa.tsx
+++ src/secret-santa/secret-santa.tsx
@@ -3,6 +3,10 @@ import ResultTable from "../results/result-table";
 import DownloadSecretSanta from "./download-secret-santa";
 import AssignSecretSanta from "./assign-secret-santa";
 import { SecretSheetInterface } from "../csv-uploader/csv-uploader.types";
+import styles from "./secret-santa.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 type SecretSantaAssignerProps = {
   employeeFile?: File;
@@ -19,17 +23,17 @@ const SecretSantaAssigner = ({
 
   return (
     <div>
-      <AssignSecretSanta
-        employees={employeeFile}
-        prevEmployees={prevEmployeeFile}
-        handleSetAssignments={handleSetAssignments}
-      />
-      <DownloadSecretSanta assignments={assignments} />
-      {assignments && (
-        <div>
-          <ResultTable assignments={assignments} />
-        </div>
-      )}
+      <div className={cx("santa-buttons")}>
+        <AssignSecretSanta
+          employees={employeeFile}
+          prevEmployees={prevEmployeeFile}
+          handleSetAssignments={handleSetAssignments}
+        />
+        <DownloadSecretSanta assignments={assignments} />
+      </div>
+      <div className={cx("santa-table")}>
+        {assignments && <ResultTable assignments={assignments} />}
+      </div>
     </div>
   );
 };
